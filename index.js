e724997b218ce2517c5dcd2df9e0b14a81a57cb1
arunV1996 <96427580+arunV1996@users.noreply.github.com>
2021-12-20 19:37:24 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,51 @@
-/**
- * @format
- */
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import { configureStore } from '@reduxjs/toolkit'
+import createSagaMiddleware from 'redux-saga'
+import reducers from './Reducers'
+import sagas from './Sagas'
 
-import { AppRegistry } from 'react-native'
-import App from './src/App'
-import { name as appName } from './app.json'
+const sagaMiddleware = createSagaMiddleware()
 
-AppRegistry.registerComponent(appName, () => App)
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['theme','language'],
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers)
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => {
+    const middlewares = getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    })
+
+    if (__DEV__ && !process.env.JEST_WORKER_ID) {
+      const createDebugger = require('redux-flipper').default
+      middlewares.push(createDebugger())
+    }
+
+    middlewares.push(sagaMiddleware)
+
+    return middlewares
+  },
+})
+
+sagaMiddleware.run(sagas)
+
+const persistor = persistStore(store)
+
+export { store, persistor }
